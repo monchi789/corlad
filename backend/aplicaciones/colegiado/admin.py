9e04275fe2fa6baeb8b33dcf6d1a8f8bebca4499
r0bBy001 <95073255+r0bBy001@users.noreply.github.com>
2024-08-04 19:15:31 -0500

    Revert "Merge branch 'development' of https://github.com/monchi789/corlad into development"
    This reverts commit 28deb5ae269770f7e86711ec9231c945dfd34e6b, reversing
    changes made to fde635cb1604cdd8521ac8a33858c1231c9289a4.

--- backend/aplicaciones/colegiado/admin.py
+++ backend/aplicaciones/colegiado/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Colegiado, Escuela, Especialidad, HistorialEducativo, EstadoColegiatura
+from .models import Colegiado, Escuela, Especialidad, HistorialEducativo
 
 # Register your models here.
 admin.site.register(Especialidad)
 admin.site.register(Escuela)
 admin.site.register(Colegiado)
 admin.site.register(HistorialEducativo)
-admin.site.register(EstadoColegiatura)
