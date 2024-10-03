012ba5efc93600aabd93cadc2164eed1e8d1aee4
Cristian Monzon <cristian-02-@live.com>
2024-10-03 11:25:15 -0500

    add.

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATIC_URL = '/static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
