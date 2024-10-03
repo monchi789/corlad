952606401d1836140704a2052f7de8a9ba5d4fbb
Cristian Monzon <cristian-02-@live.com>
2024-10-03 11:22:50 -0500

    add.

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -115,9 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Cambiado para ser más explícito
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
