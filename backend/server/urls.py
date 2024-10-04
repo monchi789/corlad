b6af1cbbc292080b0b6dbf767a3313d84df40496
Cristian Monzon <cristian-02-@live.com>
2024-10-04 12:49:14 -0500

    Cambios

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -58,5 +58,6 @@ urlpatterns = [
 ]
 
 # Servir imagenes staticas
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
