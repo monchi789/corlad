be9ec8e896aabd23b498999873a562ac0c618954
Cristian Monzon <cristian-02-@live.com>
2024-10-03 19:51:23 -0500

    Quitando el requerimiento de archivos estaticos

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -119,7 +119,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
